890af03a21bb6d01b1795ec2aa51d13b053ef0b7
Benjamin Day <benday@benday.com>
2019-08-16 10:16:28 -0400

    Added solution with solution file in it

---
+++ VsMacBug20190816/src/VsMacBug20190816.Api/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace VsMacBug20190816.Api
+{
+    public class Class1
+    {
+        // one two three four five
+    }
+}
---
+++ VsMacBug20190816/test/VsMacBug20190816.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VsMacBug20190816.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
